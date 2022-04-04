19970945d7f93fcf496d4b831ae64bd834206913
Rebekah Zhou <rebekahzhou@gmail.com>
2022-04-04 16:01:50 -0700

    attempt with rendering more info from pdf

--- hi.rb
+++ hi.rb
@@ -1,5 +1,17 @@
-require 'json'
+require 'open-uri'
+require 'pdf/reader'
 
-file = File.read('client/src/db.json')
-data_hash = JSON.parse(file)
-pp data_hash
+File.open("Item_Rel_2017-18_EOC-Math.pdf", "rb") do |io|
+    reader = PDF::Reader.new(io)
+    # puts reader.pdf_version
+    # puts reader.info
+    # puts reader.metadata
+    # puts reader.page_count
+    # reader.pages.each do |page|
+    #     puts page.fonts
+    #     puts page.text
+    #     puts page.raw_content
+    #   end
+    puts reader.page(28).text
+    # puts reader.objects.inspect
+  end
